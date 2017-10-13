134dda8e2af688924b4ffda8519778a9b2d38610
Jordan Lapid <jordanlapid@gmail.com>
2017-10-12 17:18:07 -0700

    final

--- liri.js
+++ liri.js
@@ -1,30 +1,48 @@
 //Linking keys.js file
 var keys = require("./keys");
+
+//Creating variable to store node request package link
 var request = require("request");
+
+//Creating variable to store node file system link
+var fs = require("fs");
 
 //Creating variables to store keys that are pulled from keys.js
 var spotifyKeys = keys.spotify;
 var twitterKeys = keys.twitter;
 
+//Creating variable to store data input for third node command line argument
 var command = process.argv[2];
 
+//Creating blank variable to hold search term
 var searchTerm = "";
+
+//Assigning variable to process.argv array
 var searchArray = process.argv;
 
-switch (command){
-	case "my-tweets":
-	twitter();
-	break;
-	case "spotify-this-song":
-	createSearch();
-	spotify();
-	break;
-	case "movie-this":
-	createSearch();
-	omdb();
-	break;
-	case "do-what-it-says":
-	break;
+//Initial invokation of verify command function
+verifyCommand();
+
+function verifyCommand (){
+	switch (command){
+		case "my-tweets":
+		twitter();
+		break;
+
+		case "spotify-this-song":
+		createSearch();
+		spotify();
+		break;
+
+		case "movie-this":
+		createSearch();
+		omdb();
+		break;
+
+		case "do-what-it-says":
+		doWhatItSays();
+		break;
+	}
 }
 
 
@@ -42,6 +60,8 @@ function createSearch () {
 			for (var i = 3; i < searchArray.length; i++){
 				if(searchArray.length === 4){
 					searchTerm += searchArray[i]
+				} else if (i === searchArray.length-1) {
+					searchTerm += searchArray[i]
 				} else {
 					searchTerm += searchArray[i] +"+"
 				}
@@ -49,7 +69,7 @@ function createSearch () {
 		} else {
 			searchTerm = "Mr.Nobody";
 		}
-	}
+	} 
 }
 
 function twitter (){
@@ -66,7 +86,6 @@ function twitter (){
 
 function spotify (){
 spotifyKeys.search({ type: 'track', query: searchTerm }).then(function(response) {
-
     console.log("Artist: " + response.tracks.items[0].artists[0].name + "\nAlbum: " + response.tracks.items[0].album.name + "\nSong: " + response.tracks.items[0].name + "\nURL: " + response.tracks.items[0].external_urls.spotify);
   })
   .catch(function(err) {
@@ -75,17 +94,29 @@ spotifyKeys.search({ type: 'track', query: searchTerm }).then(function(response)
 }
 
 function omdb(){
-
-	request("http://www.omdbapi.com/?t=" + searchTerm +"&y=&plot=short&apikey=40e9cece", function(error, response, body) {
-		if (!error && response.statusCode === 200) {
+	var param = "http://www.omdbapi.com/?t=" + searchTerm +"&y=&plot=short&apikey=40e9cece"
+	request(param, function(error, response, body) {
 		body = JSON.parse(body);
+		if (body.Response !== "False") {
 		console.log("Title: " + body.Title + "\nYear: " + body.Year + "\nIMDB Rating: " + body.Ratings[0].Value +  "\nRotten Tomatoes Rating: " + body.Ratings[1].Value + "\nCountry: " + body.Country +  "\nLanguage: " + body.Language +  "\nPlot: " + body.Plot +  "\nActors: " + body.Actors);
-}
+		} else {
+			console.log("Movie not found, please try again.")
+		}
 });
 
 
 }
 
 function doWhatItSays(){
-
+	fs.readFile("random.txt", "utf8", function(error, data) {
+		if (error) {
+			return console.log(error);
+		} else {
+			var dataArr = data.split(",");
+			command = dataArr[0];
+			searchArray[3] = dataArr[1].trim();
+			verifyCommand();
+		}
+	});
 }
+
